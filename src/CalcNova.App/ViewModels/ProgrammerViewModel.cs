a29115eabcf0bda4c1b0637c0027bc223343cb20
Sanskar <sanskarin@outlook.in>
2026-08-18 15:57:46 +0530

    feat(ui): add programmer mode view model

---
+++ src/CalcNova.App/ViewModels/ProgrammerViewModel.cs
@@ -0,0 +1,120 @@
+using System.Numerics;
+using System.Windows.Input;
+using CalcNova.App.Infrastructure;
+using CalcNova.Programmer;
+
+namespace CalcNova.App.ViewModels;
+
+public sealed class ProgrammerViewModel : ViewModelBase
+{
+    private string _input = "42";
+    private int _inputBase = 10;
+    private int _wordSize = 64;
+    private bool _signed = true;
+    private string _binary = "101010";
+    private string _octal = "52";
+    private string _decimal = "42";
+    private string _hexadecimal = "2A";
+    private string _bitPattern = BitwiseCalculator.ToBitString(new BigInteger(42), 64);
+    private string _interpretedValue = "42";
+    private string _errorMessage = string.Empty;
+
+    public ProgrammerViewModel()
+    {
+        ConvertCommand = new RelayCommand(_ => Convert());
+    }
+
+    public IReadOnlyList<int> CommonBases { get; } = [2, 8, 10, 16, 36];
+
+    public IReadOnlyList<int> WordSizes { get; } = [8, 16, 32, 64, 128];
+
+    public string Input
+    {
+        get => _input;
+        set => SetField(ref _input, value ?? string.Empty);
+    }
+
+    public int InputBase
+    {
+        get => _inputBase;
+        set => SetField(ref _inputBase, value);
+    }
+
+    public int WordSize
+    {
+        get => _wordSize;
+        set => SetField(ref _wordSize, value);
+    }
+
+    public bool Signed
+    {
+        get => _signed;
+        set => SetField(ref _signed, value);
+    }
+
+    public string Binary
+    {
+        get => _binary;
+        private set => SetField(ref _binary, value);
+    }
+
+    public string Octal
+    {
+        get => _octal;
+        private set => SetField(ref _octal, value);
+    }
+
+    public string Decimal
+    {
+        get => _decimal;
+        private set => SetField(ref _decimal, value);
+    }
+
+    public string Hexadecimal
+    {
+        get => _hexadecimal;
+        private set => SetField(ref _hexadecimal, value);
+    }
+
+    public string BitPattern
+    {
+        get => _bitPattern;
+        private set => SetField(ref _bitPattern, value);
+    }
+
+    public string InterpretedValue
+    {
+        get => _interpretedValue;
+        private set => SetField(ref _interpretedValue, value);
+    }
+
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        private set => SetField(ref _errorMessage, value);
+    }
+
+    public ICommand ConvertCommand { get; }
+
+    private void Convert()
+    {
+        try
+        {
+            var value = RadixConverter.Parse(Input, InputBase);
+            Binary = RadixConverter.Format(value, 2);
+            Octal = RadixConverter.Format(value, 8);
+            Decimal = RadixConverter.Format(value, 10);
+            Hexadecimal = RadixConverter.Format(value, 16);
+            BitPattern = BitwiseCalculator.ToBitString(value, WordSize);
+            var interpreted = Signed
+                ? BitwiseCalculator.ToSigned(value, WordSize)
+                : BitwiseCalculator.ToUnsigned(value, WordSize);
+            InterpretedValue = interpreted.ToString();
+            ErrorMessage = string.Empty;
+        }
+        catch (Exception exception) when (exception is FormatException or ArgumentException or OverflowException)
+        {
+            ErrorMessage = exception.Message;
+        }
+    }
+}
