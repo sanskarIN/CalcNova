f42fc652156b71e637e5023e760b9dce6822a3be
Sanskar <sanskarin@outlook.in>
2026-08-18 15:48:57 +0530

    feat(equations): define equation solution kinds

---
+++ src/CalcNova.Equations/EquationSolutionKind.cs
@@ -0,0 +1,11 @@
+namespace CalcNova.Equations;
+
+public enum EquationSolutionKind
+{
+    UniqueReal = 0,
+    TwoReal,
+    RepeatedReal,
+    ComplexPair,
+    NoSolution,
+    InfiniteSolutions
+}
