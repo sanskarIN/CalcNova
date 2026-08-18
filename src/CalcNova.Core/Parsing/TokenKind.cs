594b48b89316d3974903b71ceab7de8278bbf891
Sanskar <sanskarin@outlook.in>
2026-08-18 15:23:28 +0530

    feat(parser): define expression token kinds

---
+++ src/CalcNova.Core/Parsing/TokenKind.cs
@@ -0,0 +1,17 @@
+namespace CalcNova.Core.Parsing;
+
+public enum TokenKind
+{
+    Number = 0,
+    Identifier,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Percent,
+    Caret,
+    LeftParenthesis,
+    RightParenthesis,
+    Comma,
+    End
+}
