862a60e66e7308463e9122d0de706b0ab000bb13
Sanskar <sanskarin@outlook.in>
2026-08-18 19:14:14 +0530

    feat(programmer): wire bit-toggle button interaction

--- src/CalcNova.App/Views/Modes/ProgrammerModeView.axaml.cs
+++ src/CalcNova.App/Views/Modes/ProgrammerModeView.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using CalcNova.App.ViewModels;
 
 namespace CalcNova.App.Views.Modes;
 
@@ -8,5 +10,13 @@ public partial class ProgrammerModeView : UserControl
     public ProgrammerModeView()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void ToggleBit(object? sender, RoutedEventArgs eventArgs)
+    {
+        if (DataContext is ProgrammerViewModel viewModel && sender is Button { DataContext: ProgrammerBitCell cell })
+        {
+            viewModel.ToggleBitCommand.Execute(cell);
+        }
     }
 }
