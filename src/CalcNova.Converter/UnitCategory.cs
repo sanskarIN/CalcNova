9598a8069d078f3fa0791e771835a760f269a528
Sanskar <sanskarin@outlook.in>
2026-08-18 15:27:32 +0530

    feat(converter): define fixed unit categories

---
+++ src/CalcNova.Converter/UnitCategory.cs
@@ -0,0 +1,19 @@
+namespace CalcNova.Converter;
+
+public enum UnitCategory
+{
+    Length = 0,
+    Area,
+    Volume,
+    Mass,
+    Speed,
+    Temperature,
+    Time,
+    Data,
+    Frequency,
+    Pressure,
+    Energy,
+    Power,
+    Force,
+    Angle
+}
