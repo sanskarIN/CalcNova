b1f720bb876a3c05a9ddde3f65a88bb6333cb2f4
Sanskar <sanskarin@outlook.in>
2026-08-18 15:30:58 +0530

    test(programmer): add regression coverage for invalid radix input

---
+++ tests/CalcNova.Programmer.Tests/RadixValidationTests.cs
@@ -0,0 +1,25 @@
+using CalcNova.Programmer;
+using Xunit;
+
+namespace CalcNova.Programmer.Tests;
+
+public sealed class RadixValidationTests
+{
+    [Theory]
+    [InlineData("_")]
+    [InlineData("+")]
+    [InlineData("-")]
+    [InlineData("___")]
+    public void Parse_InputWithoutDigits_ThrowsFormatException(string input)
+    {
+        Assert.Throws<FormatException>(() => RadixConverter.Parse(input, 16));
+    }
+
+    [Theory]
+    [InlineData("2", 2)]
+    [InlineData("G", 16)]
+    public void Parse_InvalidDigitForBase_ThrowsFormatException(string input, int radix)
+    {
+        Assert.Throws<FormatException>(() => RadixConverter.Parse(input, radix));
+    }
+}
