8f42bc0d9d4776aeb18d7f99e57f232db04b4d5e
Sanskar <sanskarin@outlook.in>
2026-08-19 14:38:28 +0530

    feat(graph): define deterministic multi-series line patterns

---
+++ src/CalcNova.Graphing/GraphSeriesLinePattern.cs
@@ -0,0 +1,13 @@
+namespace CalcNova.Graphing;
+
+public enum GraphSeriesLinePattern
+{
+    Solid = 0,
+    Dash,
+    Dot,
+    DashDot,
+    DashDotDot,
+    LongDash,
+    ShortDash,
+    SparseDot
+}
