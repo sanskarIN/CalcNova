9da72094505b124d62ba3ac457cb5f85dc04232b
Sanskar <sanskarin@outlook.in>
2026-08-19 08:50:17 +0530

    feat(i18n): define semantic application string keys

---
+++ src/CalcNova.App/Localization/AppStringKey.cs
@@ -0,0 +1,49 @@
+namespace CalcNova.App.Localization;
+
+public enum AppStringKey
+{
+    AppName,
+    Tagline,
+    LocalFirst,
+
+    ModeCalculator,
+    ModeProgrammer,
+    ModeCodePoint,
+    ModeConverter,
+    ModeStatistics,
+    ModeEquations,
+    ModeMatrices,
+    ModeGraphing,
+    ModeDateTime,
+    ModeCurrency,
+    ModeHistory,
+    ModeSettings,
+    ModeAbout,
+
+    ActionEvaluate,
+    ActionPasteExpression,
+    ActionCopyResult,
+    ActionConvert,
+    ActionSwap,
+    ActionAnalyze,
+    ActionSolve,
+    ActionCopy,
+    ActionRefresh,
+    ActionSave,
+    ActionReset,
+
+    LabelExpression,
+    LabelResult,
+    LabelAngleUnit,
+    LabelWordSize,
+    LabelSignificantDigits,
+    LabelRecentPairs,
+    LabelFavoritePairs,
+    LabelTheme,
+    LabelHistoryLimit,
+
+    StatusCopied,
+    StatusReady,
+    ErrorInvalidInput,
+    ErrorClipboardUnavailable
+}
