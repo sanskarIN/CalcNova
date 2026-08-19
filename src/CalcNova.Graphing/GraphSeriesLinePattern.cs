89e4f21a2b4cc5dc7cb85469c9fa5caf1cece297
Sanskar <sanskarin@outlook.in>
2026-08-19 14:42:59 +0530

    refactor(graph): align domain line patterns with renderer catalog

--- src/CalcNova.Graphing/GraphSeriesLinePattern.cs
+++ src/CalcNova.Graphing/GraphSeriesLinePattern.cs
@@ -3,11 +3,11 @@ namespace CalcNova.Graphing;
 public enum GraphSeriesLinePattern
 {
     Solid = 0,
-    Dash,
-    Dot,
-    DashDot,
-    DashDotDot,
     LongDash,
     ShortDash,
-    SparseDot
+    Dot,
+    DashDot,
+    SparseDash,
+    DenseDash,
+    AlternatingDash
 }
